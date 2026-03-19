4ad007367280433f58315027e3b92097ebd875cd
Karel <karaps5705@gmail.com>
2026-03-19 17:09:25 +0100

    fix: cookie sameSite none pour cross-domain

--- src/app.ts
+++ src/app.ts
@@ -23,11 +23,11 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
-      httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "lax",
-    },
+  secure: true,
+  httpOnly: true,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  sameSite: "none",
+},
   })
 );
 
